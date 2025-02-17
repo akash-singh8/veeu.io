5233f0839391b5350d88b31438d24b69af8668ab
Akash Singh <developer.akash8@gmail.com>
2025-02-18 00:27:54 +0530

    component: added open-source

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,14 @@
 import styles from "@/styles/page.module.scss";
 import Landing from "@/components/Landing";
 import Features from "@/components/Features";
+import OpenSource from "@/components/OpenSource";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Landing />
       <Features />
+      <OpenSource />
     </div>
   );
 }
