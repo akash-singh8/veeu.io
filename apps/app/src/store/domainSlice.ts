f2c2ce0248c91b8fd801f0ccbdf1bca640755186
Akash Singh <developer.akash8@gmail.com>
2025-03-02 00:31:21 +0530

    update: domainSlice

--- apps/app/src/store/domainSlice.ts
+++ apps/app/src/store/domainSlice.ts
@@ -1,7 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  domain: "",
+const initialState: {
+  currDomain: string;
+  domains: string[];
+} = {
+  currDomain: "",
+  domains: [],
 };
 
 const domainSlice = createSlice({
@@ -9,10 +13,23 @@ const domainSlice = createSlice({
   initialState,
   reducers: {
     changeDomain: (state, action) => {
-      state.domain = action.payload;
+      state.currDomain = action.payload;
+    },
+    setDomains: (state, action) => {
+      state.domains = action.payload;
+    },
+    addDomain: (state, action) => {
+      state.domains.push(action.payload);
+    },
+    removeDomain: (state, action) => {
+      state.domains = state.domains.filter(
+        (domain) => domain !== action.payload
+      );
     },
   },
 });
 
-export const { changeDomain } = domainSlice.actions;
+export const { changeDomain, setDomains, addDomain, removeDomain } =
+  domainSlice.actions;
+
 export default domainSlice.reducer;
