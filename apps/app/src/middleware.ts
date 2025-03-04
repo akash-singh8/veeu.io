21329817e4d103f36dc5b99295be0fa92176e4ee
Akash Singh <developer.akash8@gmail.com>
2025-03-04 18:48:59 +0530

    middleware: handle cors and auth

--- apps/app/src/middleware.ts
+++ apps/app/src/middleware.ts
@@ -1,6 +1,35 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher("/auth");
+const isPublicApiRoute = createRouteMatcher(["/api/check"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isPublicApiRoute(req)) {
+    const origin = req.headers.get("origin") || "";
+    const allowedOrigins = ["http://localhost:3000", "https://veeu.io"];
+    const isAllowedOrigin = allowedOrigins.includes(origin);
+    const response = NextResponse.next();
+
+    if (isAllowedOrigin) {
+      response.headers.set("Access-Control-Allow-Origin", origin);
+    }
+    return response;
+  }
+
+  const { userId } = await auth();
+
+  // if user is logged and trying to access auth, redirect them to dashboard
+  if (userId && isPublicRoute(req)) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (!userId && !isPublicRoute(req)) {
+    return NextResponse.redirect(new URL("/auth?mode=sign-in", req.url));
+  }
+
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: [
